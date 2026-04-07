ed92e4616f850ca66a4294f5d548b9f9534a77b3
fernandesasher21-afk <fernandesasher21@gmail.com>
2026-04-07 23:57:31 +0530

    fix: intercept recovery hash synchronously in index.html

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Intercept recovery token in URL BEFORE Supabase removes it
-if (window.location.hash.includes("type=recovery")) {
-  sessionStorage.setItem("recovery_mode", "true");
-}
-
 createRoot(document.getElementById("root")!).render(<App />);
